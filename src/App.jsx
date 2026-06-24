ea17cceedd3f0491ba14f3474315fcc1d67f2c9b
Guilherme <n.guilhermealves@gmail.com>
2026-06-24 00:05:15 -0300

    feat: add loading spinner component and integrate loading state in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import ForecastList from "./components/ForecastList";
+import Loading from "./components/Loading";
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 
@@ -8,11 +9,13 @@ const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const CITY = "São José dos Campos, SP";
 
 function App() {
+    const [loading, setLoading] = useState(false);
     const [weather, setWeather] = useState(null);
     const [forecast, setForecast] = useState(null);
 
     useEffect(() => {
         async function fetchWeather() {
+            setLoading(true);
             try {
                 const response = await fetch(`https://api.hgbrasil.com/weather?key=${API_KEY}&format=json-cors&city_name=${CITY}`);
                 const data = await response.json();
@@ -22,6 +25,8 @@ function App() {
                 }
             } catch (error) {
                 console.error("Erro ao buscar dados da API: ", error);
+            } finally {
+                setLoading(false);
             }
         }
 
@@ -31,12 +36,16 @@ function App() {
     return (
         <div className="app-container">
             <SearchBar />
-            {weather && (
-                <>
-                    <h1>{weather.city}</h1>
-                    <WeatherCard weather={weather} />
-                    <ForecastList forecasts={forecast} />
-                </>
+            {loading ? (
+                <Loading />
+            ) : (
+                weather && (
+                    <>
+                        <h1>{weather.city}</h1>
+                        <WeatherCard weather={weather} />
+                        <ForecastList forecasts={forecast} />
+                    </>
+                )
             )}
         </div>
     );
